c6ef191419c1479b609fde4a7a4728f63cd5a60c
Pablo Acevedo Montserrat <pacevedo@redhat.com>
2024-01-26 13:59:54 +0100

    Add config definitions

--- pkg/config/ingress.go
+++ pkg/config/ingress.go
@@ -1,6 +1,31 @@
 package config
 
+type IngressStatusEnum string
+
+const (
+	StatusEnabled  = "Enabled"
+	StatusDisabled = "Disabled"
+)
+
 type IngressConfig struct {
+	Status             IngressStatusEnum   `json:"status"`
+	Policy             IngressPolicyConfig `json:"policy"`
 	ServingCertificate []byte
 	ServingKey         []byte
+}
+
+type IngressPolicyConfig struct {
+	Ports  IngressPolicyPortsConfig `json:"ports"`
+	Expose []string                 `json:"expose"`
+	Allow  []string                 `json:"allow,omitempty"`
+}
+
+type IngressPolicyPortsConfig struct {
+	Http  IngressPolicyPort `json:"http"`
+	Https IngressPolicyPort `json:"https"`
+}
+
+type IngressPolicyPort struct {
+	Status IngressStatusEnum `json:"status"`
+	Port   uint16            `json:"port"`
 }
